940819cf7fe328880b01ad3bbba790bae8d7541c
Christoffer H. Knudsen <christofferhuggerknudsen@gmail.com>
2017-09-12 06:33:43 +0200

    fixing stuff

--- wp-config.php
+++ wp-config.php
@@ -1,38 +1,34 @@
 <?php
 /**
- * The base configuration for WordPress
+ * The base configurations of the WordPress.
  *
- * The wp-config.php creation script uses this file during the
- * installation. You don't have to use the web site, you can
- * copy this file to "wp-config.php" and fill in the values.
+ * This file has the following configurations: MySQL settings, Table Prefix,
+ * Secret Keys, WordPress Language, and ABSPATH. You can find more information
+ * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
+ * wp-config.php} Codex page. You can get the MySQL settings from your web host.
  *
- * This file contains the following configurations:
- *
- * * MySQL settings
- * * Secret keys
- * * Database table prefix
- * * ABSPATH
- *
- * @link https://codex.wordpress.org/Editing_wp-config.php
+ * This file is used by the wp-config.php creation script during the
+ * installation. You don't have to use the web site, you can just copy this file
+ * to "wp-config.php" and fill in the values.
  *
  * @package WordPress
  */
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wp_lego');
+define('DB_NAME', 'christoffer_knudsen_dk_wp_lego');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'christoffer_knudsen_dk_wp_lego');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'password');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'christoffer-knudsen.dk.mysql');
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8mb4');
+define('DB_CHARSET', 'utf8mb4');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
@@ -46,24 +42,24 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'bWv|kZFw1v`s.4ln&<e6XZ/YY*m-yXiKi3w,DFSz1|8ST_4y:$sg;3$19t.;fduJ');
-define('SECURE_AUTH_KEY',  '4589o/ljB-`T|655Tm3SZx=[rhkNVTC/e44xrg=6yCf!MAj>Hg;sr(Ajl{04Dztb');
-define('LOGGED_IN_KEY',    'xm+0E5Oq|^a2(P_M40G(,t=Ymf($-~rc|4e(-+W%OPPwzZm$X(s@3qbEYF2oh6GX');
-define('NONCE_KEY',        '!&>82Gi~Eq`$@,Q*52bT3A@>iNq:50zDEl(gU~7;W)_G|/M2Q=[<A.4N@-AHv7jZ');
-define('AUTH_SALT',        'MAw=mc8o2(KkznR<pib^l#^NNU;PU75K.L~SB[`9Cq7/E&;[r.P:#Z2ONH.Lwf|j');
-define('SECURE_AUTH_SALT', 'AjIvk0s7t4B,Q(>XB1*qrO=9hAP~P;]0ugx>Mf >Q<Q;G[#H !^]$G;D1%?_b^$Y');
-define('LOGGED_IN_SALT',   '}mPhLp_| [_g07[!i{q-4a8.%[Cuh`D3k?m19d{#/ ]{H(~^U9-/U*Dlc=^A{{@1');
-define('NONCE_SALT',       'F*,l^wZE<[x4:W@^+4/*eK*Ay-8_qB#][bU|6LVsqwf77guUp&S:-}S:WgQU%iFR');
+define('AUTH_KEY',         'oI@F8j<Q,CF{=cWI1!/#<~Zvu^vyioY{8d7|uTrZb/v5<O#Z_XF0x!=j*D+=-dXf');
+define('SECURE_AUTH_KEY',  '|c9#Ro.SBJE*VxAVZD0um3ksQ$KK}Z[as~O@Qh~A66=mB0xiQvOiL@j*AV?d$)U9');
+define('LOGGED_IN_KEY',    'p$/Xg`?j:l|uF ZPFm%j[9cWq-a8pSyxrnA%O4Ll?<lo7(OeYiA/}P+f3>2|_,0_');
+define('NONCE_KEY',        'y+,0o~x!F-12b|kS6R.+4[WZjK8a^WAZ49]9Gqw&-5{9,f9[/N)3%&3^9_.sWa! ');
+define('AUTH_SALT',        '7Cc%enbNPl:@X&-b%4re- OW7J.|@4;}m-6O!$fI]?X1u(5H~D&=TWe#{fdD(E9A');
+define('SECURE_AUTH_SALT', 'wkx 1N!Cf!(LD[iqG.#@Axu0X&~$?iV|QCDl:x)vu6F6%Lg{{kHYo4(!x}u $#=_');
+define('LOGGED_IN_SALT',   'Fwnx,6&]Cll[NrAX&,`mxkN4Sj1ry10P.F7QL!rCFe #n]El<NK./^uQ [}(hxOD');
+define('NONCE_SALT',       'KF&L&thv[k){gwhYM|yo5sF/[h`XtZgW)Qri.hXFL%;4]va^q[4Ab}eWeUaz<B;a');
 
 /**#@-*/
 
 /**
  * WordPress Database Table prefix.
  *
- * You can have multiple installations in one database if you give each
- * a unique prefix. Only numbers, letters, and underscores please!
+ * You can have multiple installations in one database if you give each a unique
+ * prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix  = 'wp_';
+$table_prefix  = 'wp_';
 
 /**
  * For developers: WordPress debugging mode.
@@ -71,11 +67,6 @@ $table_prefix  = 'wp_';
  * Change this to true to enable the display of notices during development.
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
  * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the Codex.
- *
- * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
 define('WP_DEBUG', false);
 
