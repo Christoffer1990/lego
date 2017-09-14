b579dbbed86a646703732a59b20134c501ced7a5
Christoffer1990 <chris_offer5@hotmail.com>
2017-09-14 11:32:37 +0200

    added posts to homepage

--- wp-content/themes/lego/page-forside.php
+++ wp-content/themes/lego/page-forside.php
@@ -20,7 +20,18 @@ get_header(); ?>
 
 
 
+			<?php
+				while ( have_posts() ) : the_post();
 
+					get_template_part( 'template-parts/content', 'page' );
+
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+
+				endwhile; // End of the loop.
+			?>
 
 			<?php
 			//echo do_shortcode('[smartslider3 slider=2]');
