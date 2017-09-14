940ebaec1ce15cf026dfe6142cbb3566234fc7cc
Christoffer1990 <chris_offer5@hotmail.com>
2017-09-14 10:44:29 +0200

    video widget area

--- wp-content/themes/lego/functions.php
+++ wp-content/themes/lego/functions.php
@@ -128,6 +128,15 @@ function lego_widgets_init() {
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
+	register_sidebar( array(
+		'name'          => esc_html__( 'video-widget', 'lego' ),
+		'id'            => 'video-widget',
+		'description'   => esc_html__( 'Add widgets here.', 'lego' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
 
 }
 add_action( 'widgets_init', 'lego_widgets_init' );
---
+++ wp-content/themes/lego/sidebar-video.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The sidebar containing the main widget area
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package lego
+ */
+
+if ( ! is_active_sidebar( 'video-widget' ) ) {
+	return;
+}
+?>
+
+
+<div id="video-widget" class="widget-area" style="background-image:url('<?php if( get_field('video_widget_area') ):the_field('video_widget_area');endif; ?>')">
+	<div class="center">
+		<?php dynamic_sidebar( 'video-widget' ); ?>
+	</div>
+</div><!-- #secondary -->
+
+
