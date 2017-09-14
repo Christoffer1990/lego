6cefdd700c039562912383de4f6b17173f9a81da
BjoernBC <bbc55755@gmail.com>
2017-09-14 13:56:45 +0200

    video final

--- wp-content/themes/lego/sidebar-video.php
+++ wp-content/themes/lego/sidebar-video.php
@@ -14,8 +14,17 @@ if ( ! is_active_sidebar( 'video-widget' ) ) {
 
 
 <div id="video-widget" class="widget-area" style="background-image:url('<?php if( get_field('video_widget_area') ):the_field('video_widget_area');endif; ?>')">
+	
+
 	<div class="center">
-		<?php dynamic_sidebar( 'video-widget' ); ?>
+		<h1>
+			THE LEGO<sup>&#174;</sup> NINJAGO<sup>&#174;</sup> MOVIE&#8482;
+		</h1>
+		<a href="#">Se mere</a>		
+		<div id="center-video">
+			<?php dynamic_sidebar( 'video-widget' ); ?>
+		</div>
+		
 	</div>
 </div><!-- #secondary -->	
 
