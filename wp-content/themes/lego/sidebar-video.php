a1fed5c8ea10aceb77266461b588e69c47dcad61
Christoffer1990 <chris_offer5@hotmail.com>
2017-09-14 12:53:40 +0200

    ....

--- wp-content/themes/lego/sidebar-video.php
+++ wp-content/themes/lego/sidebar-video.php
@@ -15,10 +15,6 @@ if ( ! is_active_sidebar( 'video-widget' ) ) {
 
 <div id="video-widget" class="widget-area" style="background-image:url('<?php if( get_field('video_widget_area') ):the_field('video_widget_area');endif; ?>')">
 	<div class="center">
-		<h1>
-			THE LEGO&#174; NINJAGO&#174; MOVIE	&#8482;
-		</h1>
-		<a href="#"></a>
 		<?php dynamic_sidebar( 'video-widget' ); ?>
 	</div>
 </div><!-- #secondary -->	
